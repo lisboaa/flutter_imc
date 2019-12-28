d70815f8af2e2f8512556dd3f1ca4fd7e7882d53
lisboaa <douglas.lisboa@edu.unipar.br>
2019-12-28 16:42:00 -0300

    Implementado validação para campos obrigatorios.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
@@ -32,16 +33,20 @@ class _HomeState extends State<Home> {
       double imc = weight / (height * height);
       if (imc <= 18.6) {
         _infoText = "Abaixo do Peso.(${imc.toStringAsPrecision(2)})";
-      } else if (imc >= 18.5 || imc < 24.99) {
-        _infoText = "Peso normal.(${imc.toStringAsPrecision(2)})";
-      } else if (imc >= 24.9 || imc < 34.99) {
-        _infoText = "Levemente acima do peso.(${imc.toStringAsPrecision(2)})";
-      } else if (imc >= 29.9 || imc < 39.99) {
-        _infoText = "Obesidade grau 1 (severa) (${imc.toStringAsPrecision(2)})";
-      } else if (imc >= 34.9 || imc < 39.9) {
-        _infoText = "Obesidade Grau 2 (${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 18.5 && imc < 24.99) {
+        _infoText = "Peso normal, seu IMC é: (${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 24.9 && imc < 34.99) {
+        _infoText =
+            "Levemente acima do peso, seu IMC é: (${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 29.9 && imc < 39.99) {
+        _infoText =
+            "Obesidade grau 1 (severa), seu IMC é:  (${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText =
+            "Obesidade Grau 2, seu IMC é:  (${imc.toStringAsPrecision(2)})";
       } else if (imc >= 40) {
-        _infoText = "Obesidade Grau 3 (${imc.toStringAsPrecision(2)})";
+        _infoText =
+            "Obesidade Grau 3, seu IMC é:  (${imc.toStringAsPrecision(2)})";
       }
     });
   }
@@ -63,50 +68,67 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_outline, size: 120.0, color: Colors.lightBlue),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Peso (kg)",
-                  labelStyle: TextStyle(color: Colors.lightBlue)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: weightController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Altura(cm)",
-                labelStyle: TextStyle(color: Colors.lightBlue),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: Colors.lightBlue),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.lightBlue)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "O Campo peso deve ser informado.";
+                  }
+                },
               ),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: heightController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Container(
-                height: 50.0,
-                margin: const EdgeInsets.only(top: 60.0),
-                child: RaisedButton(
-                  onPressed: _calculate,
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Altura(cm)",
+                  labelStyle: TextStyle(color: Colors.lightBlue),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: heightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "O Campo peso deve ser informado.";
+                  }
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  margin: const EdgeInsets.only(top: 60.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _calculate();
+                      }
+                    },
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.lightBlue,
                   ),
-                  color: Colors.lightBlue,
                 ),
               ),
-            ),
-            Text(
-              _infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.lightBlue, fontSize: 25.0),
-            ),
-          ],
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlue, fontSize: 25.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
